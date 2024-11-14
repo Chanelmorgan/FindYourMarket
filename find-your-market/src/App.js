21963ddb37e5daf35aa5cb57f892d73a687ae018
Chanel Morgan <chanelmorgan05@icloud.com>
2024-11-14 13:09:31 +0000

    Creating home page component

--- find-your-market/src/App.js
+++ find-your-market/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className="App">
         <h1>Welcome to the React App</h1>
         <Routes>
-          <Route path="/" element={<h2>Home Page</h2>} />
+          <Route path="/" element={<Home />} />
         </Routes> 
 
         <Footer />
--- find-your-market/src/pages/Home.js
+++ find-your-market/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
 
-    return <div><h2>Welcome to the Home Page</h2></div>;
+    return <div><h2>Home Page</h2></div>;
 
   
 }
