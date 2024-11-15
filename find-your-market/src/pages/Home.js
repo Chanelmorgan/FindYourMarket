3d6f1d978490c5421b5ca9217a5ddf963825da6a
pyj21zru <106590642+gracetoxicstar02@users.noreply.github.com>
2024-11-15 13:17:11 +0000

    homepage

--- find-your-market/src/pages/Home.js
+++ find-your-market/src/pages/Home.js
@@ -3,7 +3,11 @@ import '../styles/Home.css';
 function Home() {
 
     return <div>
-    <h2 className='home-title'>Hello, Tara</h2>
+    <h2 className='home-title'>Hello, Tara!</h2>
+    <h3 className='pref-market'>Norwich Market</h3>
+    <div className="curved-box">
+        Live Crowd Level
+    </div>
     </div>; 
 }
 export default Home;
