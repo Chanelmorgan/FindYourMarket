b0d1b6aef88f93d7298bafcd64b44b678891b4d2
Chanel Morgan <chanelmorgan05@icloud.com>
2024-11-29 13:15:38 +0000

    Update Market.js

--- find-your-market/src/components/Market.js
+++ find-your-market/src/components/Market.js
@@ -1,12 +1,82 @@
-import React from "react";
-import "../styles/Market.css"; 
+import React, { useState } from 'react';
+import '../styles/index.css'; 
+import '../styles/Home.css';  
+import chartImage1 from '../assets/images/todayChart.png';
+import chartImage2 from '../assets/images/monthChart.png'; 
+import heatImage from '../assets/images/newHeat.png'; 
+import video from '../assets/images/high_flashing_2.mp4'; 
 
-const Market = () => {
-  return (
-    <div className="market-container">
-        <h1 className="market-title">Stalls for Market 1 </h1>
-    </div>
-  );
-};
+const images = [
+    chartImage1,
+    chartImage2,
+    heatImage
+];
+
+const stallsData = [
+    { name: 'Stall 1', openingTime: '8:00 AM - 6:00 PM' },
+    { name: 'Stall 2 ', openingTime: '9:00 AM - 5:00 PM' },
+    { name: 'Stall 3', openingTime: '7:00 AM - 4:00 PM' },
+    { name: 'Stall 4', openingTime: '8:00 AM - 7:00 PM' },
+];
+
+function Market() {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const handleNext = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+   
+    const handlePrevious = () => {
+        setCurrentImageIndex(
+            (prevIndex) => (prevIndex - 1 + images.length) % images.length
+        );
+    };
+
+    return (
+        <div className="home-container">
+            <h2 className="home-title">Hello, Tara!</h2>
+            <h3 className="pref-market">Example Market</h3>
+            
+     
+            <div className="curved-box">
+  <div className="content-container">
+    <span className="crowd-level-title">Live Crowd Level</span>
+    <video
+        className="video"
+        src={video}
+        type="video/mp4"
+        loop
+        muted
+        autoPlay
+        playsInline
+      />
+  </div>
+</div>
+
+            {/* Slideshow */}
+            <div className="slideshow-container">
+                <img src={images[currentImageIndex]} alt="Market" className="slideshow-image" />
+                
+                {/* Navigation Arrows */}
+                <div className="arrow-left" onClick={handlePrevious}>
+                    &lt; {/* Left arrow */}
+                </div>
+                <div className="arrow-right" onClick={handleNext}>
+                    &gt; {/* Right arrow */}
+                </div>
+            </div>
+
+            <div className="stall-list">
+                {stallsData.map((stall, index) => (
+                    <div key={index} className="stall-item">
+                        <span className="stall-name">{stall.name}</span>
+                        <span className="opening-time">{stall.openingTime}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
 
 export default Market;
