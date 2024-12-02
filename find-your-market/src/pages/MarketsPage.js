d3dd029cf5d14724efd89a07b93f4e2f7f8a8719
Grace Johnson <pyj21zru@uea.ac.uk>
2024-12-02 20:37:01 +0000

    Markets Page css

--- find-your-market/src/pages/MarketsPage.js
+++ find-your-market/src/pages/MarketsPage.js
@@ -10,7 +10,6 @@ const Markets = () => {
     { id: 1, name: "Market 1", logo: "https://banner2.cleanpng.com/20180403/aww/avhnjhtse.webp", link: "/Market" },
     { id: 2, name: "Market 2", logo: "https://banner2.cleanpng.com/20180403/aww/avhnjhtse.webp", link: "/Market" },
     { id: 3, name: "Market 3", logo: "https://banner2.cleanpng.com/20180403/aww/avhnjhtse.webp", link: "/Market" },
-    { id: 4, name: "Market 4", logo: "https://banner2.cleanpng.com/20180403/aww/avhnjhtse.webp", link: "/Market" },
   ];
 
   const mapURL = `https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12201.761091040038!2d1.2958384067885549!3d52.630169007188684!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9e3e6d16bcae9%3A0x4dbfb1c1acd050a6!2sNorwich%20Market!5e0!3m2!1sen!2suk!4v1731665543326!5m2!1sen!2suk`;
@@ -41,7 +40,7 @@ const Markets = () => {
           </div>
         ))}
       </section>
-
+      <div className="map-title">Locations</div>
       <section className="markets-map">
         <iframe
           title="Market Locations"
